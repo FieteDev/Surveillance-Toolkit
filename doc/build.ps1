4fffaf74395c708033d5b0145eea3d64775025e8
Brar Piening <brar.piening@charite.de>
2023-12-14 11:16:24 +0100

    Add spanish preview watermark

--- doc/build.ps1
+++ doc/build.ps1
@@ -239,7 +239,7 @@ foreach ($targetCulture in $targetCultures)
         Write-Information "Creating Open XML (docx)"
         if (Test-RebuildRequired $buildDir/NeoIPC-Core-Protocol$langSuffix.xml $buildDir/NeoIPC-Core-Protocol$langSuffix.adoc) {
             Write-Verbose "Creating DocBook xml"
-            asciidoctor -a $revRemark -a $revDate --backend docbook --warnings --trace --failure-level WARN --destination-dir $buildDir --out-file NeoIPC-Core-Protocol$langSuffix.xml $buildDir/NeoIPC-Core-Protocol$langSuffix.adoc
+            asciidoctor -a $revRemark -a $revDate -a mathematical-format=svg -r asciidoctor-mathematical --backend docbook --warnings --trace --failure-level WARN --destination-dir $buildDir --out-file NeoIPC-Core-Protocol$langSuffix.xml $buildDir/NeoIPC-Core-Protocol$langSuffix.adoc
         }
         if (Test-RebuildRequired $outDir/img/NeoIPC-Core-Decision-Flow$langSuffix.docx $buildDir/NeoIPC-Core-Decision-Flow$langSuffix.xml @(
             "$buildImgDir/NeoIPC-Core-Decision-Flow$langSuffix.svg",
